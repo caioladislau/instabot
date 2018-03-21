74ffca8df44fdac4104f26e78ea809a6dc8f505f
Caio Ladislau <caio@ladislau.me>
2018-03-21 11:24:02 -0300

    Remove fake_useragent

--- src/instabot.py
+++ src/instabot.py
@@ -158,8 +158,6 @@ class InstaBot:
         self.follows_db = sqlite3.connect(database_name, timeout=0, isolation_level=None)
         self.follows_db_c = self.follows_db.cursor()
         check_and_update(self)
-        fake_ua = UserAgent()
-        self.user_agent = check_and_insert_user_agent(self, str(fake_ua.random))
         self.bot_start = datetime.datetime.now()
         self.start_at_h = start_at_h
         self.start_at_m = start_at_m
